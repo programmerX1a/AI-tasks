9922fe09ad3188172500da19d46402bb190751c6
Abdullah Ahmed Mohammed <sandmansiwa@gmail.com>
2026-08-02 01:54:47 +0000

    Task 2.1

--- Block_2/Task_2_1.py
+++ Block_2/Task_2_1.py
@@ -3,4 +3,13 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df=pd.read_csv("result.csv")
+
+results=pd.read_csv("results.csv")
+def decade_map(date):
+    year=int(date.split("-")[0]) 
+    return year//10 
+results["decade"]=results["date"].map(decade_map)
+results["draw"]=results["home_score"]==results["away_score"]
+results["Winning margin"]=abs(results["home_score"]-results["away_score"])
+results["goals"]=results["home_score"]+results["away_score"]
+print(results.groupby("decade")[["draw","Winning margin","goals"]].mean())
