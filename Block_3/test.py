80fefc55486187a6fc609e1004b8c97b1e6b5de2
Abdullah Ahmed Mohammed <sandmansiwa@gmail.com>
2026-08-07 14:44:26 +0000

    edit

--- Block_3/test.py
+++ Block_3/test.py
@@ -8,20 +8,37 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
+from xgboost import XGBRegressor
 df=pd.read_csv("train.csv")
-
-x=pd.get_dummies(df.drop(columns= [
+df_test=pd.read_csv("test.csv")
+x_train=pd.get_dummies(df.drop(columns= [
     "Id",
     "player_id",
     "player_name",
     "match_id",
     "match_date",
     "player_rating"
+    ,"performance_score"
 ]
 ),drop_first=True)
-y=df["player_rating"]
-x_train,x_test,y_train,y_test=train_test_split(x,y,random_state=42,test_size=0.2)
-model=RandomForestRegressor(n_estimators=200,random_state=42,max_depth=7)
+
+x_test=pd.get_dummies(df_test.drop(columns= [
+    "Id",
+    "player_id",
+    "player_name",
+    "match_id",
+    "match_date"
+    ,"performance_score"
+]
+),drop_first=True)
+x_test = x_test.reindex(columns=x_train.columns, fill_value=0)
+x_test["weighted_assists"]=x_test["assists"]-x_test["expected_assists_xa"]
+x_test["weighted_goals"]=x_test["goals"]-x_test["expected_goals_xg"]
+x_train["weighted_assists"]=x_train["assists"]-x_train["expected_assists_xa"]
+x_train["weighted_goals"]=x_train["goals"]-x_train["expected_goals_xg"]
+y_train=df["player_rating"]
+model=RandomForestRegressor(n_estimators=400,random_state=42,max_depth=10) #This was the best model 
 model.fit(x_train, y_train)
 y_pred = model.predict(x_test)
-print(mean_squared_error(y_pred,y_test))
+csv_data=pd.DataFrame({"Id":df_test.iloc[x_test.index]["Id"],"Player Rating":y_pred})
+csv_data.to_csv("result.csv",mode="a",header=True,index=False)
